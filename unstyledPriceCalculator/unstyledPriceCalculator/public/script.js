7902f6ac974b369a86dcb7b42e0d61b0f1a1df66
SOC-CM4025 <frateril.smurf@gmail.com>
2025-08-08 09:15:30 +0100

    feur

--- unstyledPriceCalculator/unstyledPriceCalculator/public/script.js
+++ unstyledPriceCalculator/unstyledPriceCalculator/public/script.js
@@ -1,9 +1,11 @@
 function calculatePrice() {
     var s = document.getElementById("salary").value;
     var d = document.getElementById("days").value;
+    var n = document.getElementById("quoteName").value;
     console.log("Calculating price")
     console.log(s)
     console.log(d)
+    console.log(n)
     let finalPrice = 0;
     dailyRate = s/365;
     Price = dailyRate * d;
--- unstyledPriceCalculator/unstyledPriceCalculator/server.js
+++ unstyledPriceCalculator/unstyledPriceCalculator/server.js
@@ -1,3 +1,6 @@
+const { MongoClient } = require("mongodb")
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri);
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -6,16 +9,61 @@ const PORT = 8080;
 // Sert les fichiers statiques (HTML, CSS, JS, etc.) depuis le dossier "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Exemple d'API backend (facultatif)
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello depuis le backend Node.js !' });
+app.get('/api', function(req, res){
+  res.send("Yes we have an API")
 });
 
-app.listen(PORT, () => {
-  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+app.get('/api/getPrice', function(req, res){
+  var s = req.query.salary;
+  var d = req.query.days;
+  var n = req.query.quoteName;
+  console.log("Calculating price")
+  console.log(s)
+  console.log(d)
+  console.log(n)
+  let finalPrice = 0;
+  dailyRate = s/365;
+  Price = dailyRate * d;
+  finalPrice = Math.round(Price /50) * 50;
+  
+  console.log("Storing quote : "+n+", "+s+", "+d)
+  console.log("Mongo URI is "+uri)
+  
+  async function run() {
+    try{
+      var dbo = client.db("mydb");
+      var myobj = { quoteName: n, salary: s, days: d};
+      await dbo.collection("quotes").insertOne(myobj, function(err, res) {
+        if (err) {
+          console.log(err);
+          throw err;
+        }
+        console.log("1 quote inserted")
+      });
+      
+    }
+    finally{
+      res.send(`Final price: ${finalPrice}, stored in ${n}`);
+    // await client.close();
+  }
+    }
+  run().catch(console.dir);
+  
+  // res.send("stored in "+n)
+});
+
+app.get('/api/storeQuote', function(req, res){
+  var s = req.query.salary;
+  var d = req.query.days;
+  var n = req.query.quoteName;
 });
 
 // Middleware 404 : doit être après toutes les autres routes
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
